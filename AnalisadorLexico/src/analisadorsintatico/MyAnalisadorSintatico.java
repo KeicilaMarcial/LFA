c6846ae243a3376c34b32361bd23845a8e57707d
anaflaviafrontino <33136783+anaflaviafrontino@users.noreply.github.com>
2018-06-25 19:17:09 -0300

    merge

---
+++ AnalisadorLexico/src/analisadorsintatico/MyAnalisadorSintatico.java
@@ -0,0 +1,44 @@
+package analisadorsintatico;
+
+/**
+ *
+ * @author Ana Flávia
+ */
+public class MyAnalisadorSintatico extends AnalisadorSintatico {
+	
+	public MyAnalisadorSintatico(String _nomeArquivoEntrada) {
+		super(_nomeArquivoEntrada);
+	}
+	public void inicio() {
+		corpo(); 
+		reconhece(Token.EOF);
+	}
+	public void corpo() {
+		if(proxTokenIs(Token.IDENT)) {
+			comandoAtribuicao();
+			reconhece(Token.PT_VIRG);
+			corpo();
+		}
+		else if(proxTokenIs(Token.EOF))
+			;
+		else {
+			Token[] tokensEsperados = {Token.IDENT,Token.EOF};
+			throw new ErroSintatico(this.scanner.tokenReconhecido,tokensEsperados);
+		}
+	}
+	public void comandoAtribuicao() {
+		reconhece(Token.IDENT);
+		reconhece(Token.ATRIB);
+		exp();
+	}
+	public void exp() {
+		if(proxTokenIs(Token.NUM)) 
+			leProxToken();
+		else if(proxTokenIs(Token.IDENT)) 
+			leProxToken();
+		else {
+			Token[] tokensEsperados = {Token.NUM,Token.IDENT};
+			throw new ErroSintatico(this.scanner.tokenReconhecido,tokensEsperados);
+		}
+	}
+}
